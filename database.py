d1334a74ef89982e683dd3e7b084e9c60db43820
Carnage098 <polairebleu7@gmail.com>
2026-06-07 01:21:59 +0200

    Create database.py

---
+++ database.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+def init_db():
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS joueurs (
+        user_id TEXT PRIMARY KEY,
+        pseudo TEXT NOT NULL,
+        points INTEGER DEFAULT 1000,
+        victoires INTEGER DEFAULT 0,
+        defaites INTEGER DEFAULT 0,
+        serie INTEGER DEFAULT 0,
+        grade TEXT DEFAULT 'Or',
+        teromik_coins INTEGER DEFAULT 0
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+  
