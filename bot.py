dfa807d1bf41779aee239e1e7ff7434371787c3a
Carnage098 <polairebleu7@gmail.com>
2026-06-07 21:13:51 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -181,10 +181,10 @@ async def inscription(interaction: discord.Interaction):
     )
 
    
-    conn.commit()
-cursor.execute("SELECT COUNT(*) FROM joueurs")
-print("JOUEURS :", cursor.fetchone()[0])
-await interaction.followup.send(
+conn.commit()
+    cursor.execute("SELECT COUNT(*) FROM joueurs")
+    print("JOUEURS :", cursor.fetchone()[0])
+    await interaction.followup.send(
     f"⚔️ Bienvenue sur TeRom-Brawl !\n\n"
     f"👤 Joueur : {pseudo}\n\n"
     f"🎖️ Grade : Recrue\n"
