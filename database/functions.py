b2bd789687c8f143d997322c554afaa3304ec47f
Carnage098 <polairebleu7@gmail.com>
2026-06-07 10:50:58 +0200

    Update functions.py

--- database/functions.py
+++ database/functions.py
@@ -1,1 +1,45 @@
+import sqlite3
+
+def get_conn():
+    return sqlite3.connect("database.db")
+
+def get_joueur(user_id):
+    conn = get_conn()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "SELECT * FROM joueurs WHERE user_id = ?",
+        (str(user_id),)
+    )
+
+    joueur = cursor.fetchone()
+
+    conn.close()
+
+    return joueur
+
+def ajouter_joueur(user_id, pseudo):
+    conn = get_conn()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        INSERT INTO joueurs (user_id, pseudo)
+        VALUES (?, ?)
+    """, (str(user_id), pseudo))
+
+    conn.commit()
+    conn.close()
+
+def update_points(user_id, points):
+    conn = get_conn()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        UPDATE joueurs
+        SET points = ?
+        WHERE user_id = ?
+    """, (points, str(user_id)))
+
+    conn.commit()
+    conn.close()
 
