33e3e6ab3241d310ba0949e9549a76a4041cd6ae
Carnage098 <polairebleu7@gmail.com>
2026-06-07 12:05:58 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import discord
 
 from discord.ext import commands
 from discord import app_commands
-
+from datetime import datetime, timedelta
 TOKEN = os.getenv("DISCORD_TOKEN")
 
 intents = discord.Intents.default()
